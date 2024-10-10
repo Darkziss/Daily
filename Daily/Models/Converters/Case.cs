b47bc731f5ffbfdebed181fb1a7fbec746a0e220
Darkziss <aserboom9@gmail.com>
2024-10-10 22:46:13 +0300

    Fix TaskEditPage crush bug

---
+++ Daily/Models/Converters/Case.cs
@@ -0,0 +1,9 @@
+﻿
+namespace Daily
+{
+    public class Case
+    {
+        public object? Key { get; set; }
+        public object? Value { get; set; }
+    }
+}
---
+++ Daily/Models/Converters/SwitchConverter.cs
@@ -0,0 +1,43 @@
+﻿using System.Globalization;
+
+namespace Daily
+{
+    public class SwitchConverter : IValueConverter
+    {
+        public object? Default { get; set; }
+        public List<Case> Cases { get; set; }
+
+        public SwitchConverter()
+        {
+            Cases = new List<Case>();
+        }
+        
+        public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+        {
+            if (value == null) return null;
+            
+            Case? pair = null;
+
+            foreach (var c in Cases)
+            {
+                if (value.Equals(c.Key)) pair = c;
+            }
+
+            return pair == null ? Default : pair.Value;
+        }
+
+        public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+        {
+            if (value == null) return null;
+            
+            Case? pair = null;
+
+            foreach (var c in Cases)
+            {
+                if (value.Equals(c.Value)) pair = c;
+            }
+
+            return pair == null ? Default : pair.Value;
+        }
+    }
+}
