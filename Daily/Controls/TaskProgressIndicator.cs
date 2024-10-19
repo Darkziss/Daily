4a51985e92cad61c0ac5c7957169f1687a8789d7
Darkziss <aserboom9@gmail.com>
2024-10-19 22:57:22 +0300

    Add prototype of Task Progress Indicator

---
+++ Daily/Controls/TaskProgressIndicator.cs
@@ -0,0 +1,27 @@
+﻿using Daily.Drawables;
+
+namespace Daily.Controls
+{
+    public class TaskProgressIndicator : GraphicsView
+    {
+        private readonly TaskProgressIndicatorDrawable _indicatorDrawable;
+
+        public Color IndicatorColor
+        {
+            get { return _indicatorDrawable.IndicatorColor; }
+            set { OnIndicatorColorChanged(value); }
+        }
+
+        public TaskProgressIndicator()
+        {
+            _indicatorDrawable = new TaskProgressIndicatorDrawable();
+            Drawable = _indicatorDrawable;
+        }
+
+        private void OnIndicatorColorChanged(object color)
+        {
+            _indicatorDrawable.IndicatorColor = (Color)color;
+            Invalidate();
+        }
+    }
+}
---
+++ Daily/Drawables/TaskProgressIndicatorDrawable.cs
@@ -0,0 +1,20 @@
+﻿
+namespace Daily.Drawables
+{
+    public class TaskProgressIndicatorDrawable : BindableObject, IDrawable
+    {
+        public Color IndicatorColor { get; set; } = Colors.White;
+
+        private const float ellipseSize = 20f;
+        
+        public void Draw(ICanvas canvas, RectF dirtyRect)
+        {
+            canvas.FillColor = IndicatorColor;
+
+            float x = (dirtyRect.Width / 2f) - (ellipseSize / 2f);
+            float y = (dirtyRect.Height / 2f) - (ellipseSize / 2f);
+
+            canvas.FillEllipse(x, y, ellipseSize, ellipseSize);
+        }
+    }
+}
