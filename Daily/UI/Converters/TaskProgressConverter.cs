d4309baa00dfc5e3596381a0dbd0f22afc22b32f
Darkziss <aserboom9@gmail.com>
2024-11-17 18:07:58 +0300

    Add #90

---
+++ Daily/UI/Converters/TaskProgressConverter.cs
@@ -0,0 +1,24 @@
+﻿using System.Globalization;
+
+namespace Daily.Converters
+{
+    public class TaskProgressConverter : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (values == null) return null!;
+            else if (values[0] is not int) return null!;
+            else if (values[1] is not int) return null!;
+
+            int repeatCount = (int)values[0];
+            int targetRepeatCount = (int)values[1];
+
+            return $"{repeatCount}/{targetRepeatCount}";
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            return null!;
+        }
+    }
+}
