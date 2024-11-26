9b1ea74e994b49fdab3f364b9f4445e2a6083f56
Darkziss <aserboom9@gmail.com>
2024-11-26 11:41:01 +0300

    Implement #100

--- Daily/Tasks/TaskStorage.cs
+++ Daily/Tasks/TaskStorage.cs
@@ -17,7 +17,7 @@ namespace Daily.Tasks
         private bool ShouldSortСonditionalTasks => СonditionalTasks.Count > 1;
 
         private const int maxGeneralTaskCount = 10;
-        private const int maxConditionalTaskCount = 5;
+        private const int maxConditionalTaskCount = 7;
 
         private const string maxGeneralTasksExceptionText = "Already created max amount of general tasks";
         private const string maxConditionalTasksExceptionText = "Already created max amount of conditional tasks";
--- Daily/ViewModels/MainPageViewModel.cs
+++ Daily/ViewModels/MainPageViewModel.cs
@@ -11,7 +11,10 @@ namespace Daily.ViewModels
 
         [ObservableProperty] private bool _canNavigate = false;
 
-        private readonly TaskStorage _taskStorage;
+        private readonly IReadOnlyList<GeneralTask> _generalTasks;
+
+        private readonly int[] _priorityCounts = new int[4];
+        private readonly bool[] _tasksLeft = new bool[4];
 
         private static readonly Color dailyColor = Color.FromRgba("edd8c3");
         private static readonly Color mandatoryColor = Color.FromRgba("edc3c3");
@@ -25,11 +28,11 @@ namespace Daily.ViewModels
 
         private const string defaultStatusText = "Загрузка..";
         private const string emptyStatusText = "Задач нет";
-        private const string allDoneStatusText = "Все задачи выполнены!";
+        private const string completedStatusText = "Все задачи выполнены!";
 
         public MainPageViewModel(TaskStorage taskStorage)
         {
-            _taskStorage = taskStorage;
+            _generalTasks = taskStorage.GeneralTasks;
             
             GoToTaskPage = new Command(
             execute: async () =>
@@ -54,61 +57,59 @@ namespace Daily.ViewModels
 
         private void SetTaskProgressStatusText()
         {
-            IReadOnlyList<GeneralTask> tasks = _taskStorage.GeneralTasks;
+            IReadOnlyList<GeneralTask> tasks = _generalTasks;
             bool isEmpty = tasks.Count == 0;
 
             if (isEmpty)
             {
                 StatusText = emptyStatusText;
                 StatusColor = emptyColor;
+
                 return;
             }
-
-            Span<int> priorityCounts = stackalloc int[4];
-            Span<bool> tasksLeft = stackalloc bool[4];
 
             for (int i = 0; i < tasks.Count; i++)
             {
                 var task = tasks[i];
                 int index = (int)task.Priority;
 
-                if (!task.IsCompleted) priorityCounts[index]++;
+                if (!task.IsCompleted) _priorityCounts[index]++;
 
-                tasksLeft[index] = priorityCounts[index] > 0;
+                _tasksLeft[index] = _priorityCounts[index] > 0;
             }
 
-            if (tasksLeft[1])
+            if (_tasksLeft[1])
             {
-                int mandatoryCount = priorityCounts[1];
+                int mandatoryCount = _priorityCounts[1];
 
                 StatusText = mandatoryCount == 1 ? $"Осталась 1 обязательная задача"
                     : $"Осталось {mandatoryCount} обязательных задач";
                 StatusColor = mandatoryColor;
             }
-            else if (tasksLeft[2])
+            else if (_tasksLeft[2])
             {
-                int importantCount = priorityCounts[2];
+                int importantCount = _priorityCounts[2];
 
                 StatusText = importantCount == 1 ? $"Осталась 1 важная задача"
                     : $"Осталось {importantCount} важных задач";
                 StatusColor = importantColor;
             }
-            else if (tasksLeft[0])
+            else if (_tasksLeft[0])
             {
-                int dailyCount = priorityCounts[0];
+                int dailyCount = _priorityCounts[0];
 
                 StatusText = dailyCount == 1 ? $"Осталась 1 ежедневная задача"
                     : $"Осталось {dailyCount} ежедневных задач";
                 StatusColor = dailyColor;
             }
-            else if (tasksLeft[3])
+            else if (_tasksLeft[3])
             {
                 StatusText = "Выполнены все задачи, кроме обычных";
                 StatusColor = completedColor;
             }
             else
             {
-                StatusText = allDoneStatusText;
+                StatusText = completedStatusText;
                 StatusColor = completedColor;
             }
         }
