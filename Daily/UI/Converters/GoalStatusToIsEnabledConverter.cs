a76bfd113d898f8145ae66eacc964a75f645efb3
Darkziss <aserboom9@gmail.com>
2025-06-14 14:32:13 +0300

    Add GoalStatusToIsEnabledConverter

---
+++ Daily/UI/Converters/GoalStatusToIsEnabledConverter.cs
@@ -0,0 +1,25 @@
+﻿using System.Globalization;
+using Daily.Tasks;
+
+namespace Daily.Converters
+{
+    public class GoalStatusToIsEnabledConverter : IValueConverter
+    {
+        private const GoalStatus DisabledGoalStatus = GoalStatus.None;
+        
+        public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+        {
+            if (value is not GoalStatus) return null;
+
+            GoalStatus status = (GoalStatus)value;
+
+            if (status == DisabledGoalStatus) return false;
+            else return true;
+        }
+
+        public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+        {
+            return null;
+        }
+    }
+}
