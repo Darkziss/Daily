1446a5ea5d7260ce472da3fca1be66843cc2174e
Darkziss <aserboom9@gmail.com>
2025-07-01 23:03:22 +0300

    Create DataFolderPath
    Move Data folder path constant to specified object

---
+++ Daily/Data/DataFolderPath.cs
@@ -0,0 +1,13 @@
+﻿using AndroidApplication = Android.App.Application;
+using AndroidEnvironment = Android.OS.Environment;
+
+namespace Daily.Data
+{
+    public static class DataFolderPath
+    {
+        private static readonly string _path = AndroidApplication.Context.
+                GetExternalFilesDir(AndroidEnvironment.DirectoryDocuments)!.AbsolutePath;
+
+        public static string Path => _path;
+    }
+}
