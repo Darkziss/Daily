dc8101c9db3809abfc59324765445a90ea35e7ad
Agshin Shirinov <aserboom9@gmail.com>
2025-07-09 21:26:11 +0300

    Add Unit of Work interface

---
+++ Daily/Data/IUnitOfWork.cs
@@ -0,0 +1,19 @@
+﻿using Daily.Thoughts;
+using Daily.Diary;
+using Daily.Tasks;
+
+namespace Daily.Data
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        public IFileRepository<Goal> GoalRepository { get; }
+
+        public IFileRepository<ICollection<GeneralTask>> GeneralTaskRepository { get; }
+
+        public IFileRepository<ICollection<ConditionalTask>> ConditionalTaskRepository { get; }
+
+        public IRepository<Thought> ThoughtRepository { get; }
+
+        public IRepository<DiaryRecord> DiaryRecordRepository { get; }
+    }
+}
