a9269c6f0b80d0a3743382461c40c58c6711ec86
Darkziss <aserboom9@gmail.com>
2025-09-09 21:30:07 +0300

    Remove routes to Thought and Diary pages

--- Daily/AppShell.xaml.cs
+++ Daily/AppShell.xaml.cs
@@ -12,12 +12,6 @@ namespace Daily
             Routing.RegisterRoute(nameof(TaskPage), typeof(TaskPage));
             Routing.RegisterRoute(nameof(GoalEditPage), typeof(GoalEditPage));
             Routing.RegisterRoute(nameof(TaskEditPage), typeof(TaskEditPage));
-
-            Routing.RegisterRoute(nameof(ThoughtPage), typeof(ThoughtPage));
-            Routing.RegisterRoute(nameof(ThoughtEditPage), typeof(ThoughtEditPage));
-
-            Routing.RegisterRoute(nameof(DiaryRecordPage), typeof(DiaryRecordPage));
-            Routing.RegisterRoute(nameof(DiaryRecordEditPage), typeof(DiaryRecordEditPage));
         }
     }
 }
